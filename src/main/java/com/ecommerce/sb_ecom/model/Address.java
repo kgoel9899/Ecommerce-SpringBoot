3047f66197da87aff1c7e608b4ccd2a2a2ef0cfa
Kshitij Goel <kshitijg9899@gmail.com>
2025-01-29 11:58:37 +0530

    Adding users and roles entities

---
+++ src/main/java/com/ecommerce/sb_ecom/model/Address.java
@@ -0,0 +1,51 @@
+package com.ecommerce.sb_ecom.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must be atleast 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be atleast 5 characters")
+    private String building;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must be atleast 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "City name must be atleast 2 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be atleast 2 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "City name must be atleast 6 characters")
+    private String pincode;
+
+    @ManyToMany(mappedBy = "addresses")
+    @ToString.Exclude
+    private List<User> users = new ArrayList<>();
+}
---
+++ src/main/java/com/ecommerce/sb_ecom/model/Role.java
@@ -0,0 +1,23 @@
+package com.ecommerce.sb_ecom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer id;
+
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING) // enum will be persisted as string in the DB (default is integer)
+    @Column(length = 20)
+    private AppRole roleName;
+}
